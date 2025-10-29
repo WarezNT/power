075ff7884f22cb0faa98b48fa78b954fa61dde11
WarezNT <umv@mivux.ro>
2025-10-29 10:03:30 +0200

    Complete React app setup for GitHub Pages deployment

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import TransformerFuseCalculator from './transformer_fuse_calculator.tsx'
+
+function App() {
+  return (
+    <div className="App">
+      <TransformerFuseCalculator />
+    </div>
+  )
+}
+
+export default App
